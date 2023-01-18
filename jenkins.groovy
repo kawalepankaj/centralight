711df12e038153f92766e3917dc5105785460822
pankaj <pankajkawale225@gmail.com>
2023-01-17 16:56:30 -0800

    pipeline code

---
+++ jenkins.groovy
@@ -0,0 +1,25 @@
+pipeline {
+    agent any
+    stages {
+    stage('git pull') {
+        steps {
+            echo 'git pull from origin'
+        }
+    }
+     stage('build') {
+         steps {
+              echo 'sudo apt update -y build'
+            }
+        }
+        stage('test') {
+            steps {
+                echo 'sudo apt update -y test'
+            }
+        }
+        stage('deploy') {
+            steps {
+                echo 'sudo apt update -y deploy'
+            }
+        }            
+    }
+}
