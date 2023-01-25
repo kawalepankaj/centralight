2a48f715a3a815bf956fc407ba8adbeea1ef66fe
pankaj <pankajkawale225@gmail.com>
2023-01-25 14:36:56 -0800

    10th commit

--- aish.groovy
+++ aish.groovy
@@ -22,7 +22,7 @@ pipeline {
                 #curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
                 #unzip awscliv2.zip
                 #sudo ./aws/install
-                #sudo apt-get install unzip -y
+                sudo apt-get install unzip -y
                 sudo apt install awscli -y
                 aws s3 ls
                 sudo mv /var/lib/jenkins/workspace/tomcat-server/target/studentapp-2.2-SNAPSHOT.war /var/lib/jenkins/student-${BUILD_ID}.war
