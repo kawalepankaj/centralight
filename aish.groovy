4de459879c1310b62434665d09c73e703a7cf448
pankaj <pankajkawale225@gmail.com>
2023-01-25 14:51:26 -0800

    10th commit

--- aish.groovy
+++ aish.groovy
@@ -19,9 +19,6 @@ pipeline {
             steps { 
                 sh '''
                 sudo apt update -y
-                #curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-                #unzip awscliv2.zip
-                #sudo ./aws/install
                 sudo apt-get install unzip -y
                 sudo apt install awscli -y
                 aws s3 ls
