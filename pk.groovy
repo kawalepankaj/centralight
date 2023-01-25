0aa5064691299a6ca5c01cbfe8a4a06ee1ddf07a
pankaj <pankajkawale225@gmail.com>
2023-01-24 21:42:00 -0800

    4th commit

--- pk.groovy
+++ pk.groovy
@@ -39,6 +39,7 @@ pipeline {
                         #curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
                         #unzip awscliv2.zip
                         #sudo ./aws/install
+                        sudo apt install awscli -y
                         aws s3 cp s3://tomcat-installation/student-${BUILD_ID}.war .
                         sudo ls
                         curl -O https://dlcdn.apache.org/tomcat/tomcat-8/v8.5.85/bin/apache-tomcat-8.5.85.tar.gz
